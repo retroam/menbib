7150b8fab690ed1e877d210981b46af14d8e9094
Your Name <retroam@yahoo.com>
2014-07-14 12:16:32 -0400

    Create factories.py

---
+++ tests/factories.py
@@ -0,0 +1,24 @@
+""" Factory boy factories for the Mendeley addon."""
+
+from factory import SubFactory, Sequence
+from tests.factories import ModularOdmFactory, UserFactory, ProjectFactory
+
+from website.addons.menbib.model import (
+    AddonMenbibUserSettings, AddonMenbibNodeSettings
+)
+
+
+class MenbibUserSettingsFactory(ModularOdmFactory):
+    FACTORY_FOR = AddonMenbibUserSettings
+
+    owner = SubFactory(UserFactory)
+    oauth_access_token = Sequence(lambda n: 'abcdef{0}'.format(n))
+
+
+class MendeleyNodeSettingsFactory(ModularOdmFactory):
+    FACTORY_FOR = AddonMenbibNodeSettings
+
+    owner = SubFactory(ProjectFactory)
+    user_settings = SubFactory(MenbibUserSettingsFactory)
+    folder = 'COS papers'
+
